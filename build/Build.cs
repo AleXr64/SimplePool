fe1f203d71515fa19b4a13d9949a31c3801ebc39
AlexR <alex91_@mail.ru>
2020-02-24 02:04:10 +0300

    Setup nuget

--- build/Build.cs
+++ build/Build.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Linq;
 using Nuke.Common;
+using Nuke.Common.CI;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
@@ -8,58 +7,96 @@ using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
-class Build : NukeBuild
+class Build: NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
-    public static int Main () => Execute<Build>(x => x.Compile);
-
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
+    [Parameter("NuGet Api Key")] readonly string NugetAPIKey;
+
     [Solution] readonly Solution Solution;
- //   [GitRepository] readonly GitRepository GitRepository;
- //   [GitVersion] readonly GitVersion GitVersion;
+    [Parameter("NuGet Source for Packages")] readonly string Source = "https://api.nuget.org/v3/index.json";
+
+    [Parameter] string NugetApiKey;
 
     AbsolutePath OutputDirectory => RootDirectory / "output";
 
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-            EnsureCleanDirectory(OutputDirectory);
-        });
+    Target Clean =>
+        _ => _
+            .Before(Restore)
+            .Executes(() =>
+             {
+                 EnsureCleanDirectory(OutputDirectory);
+             });
 
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-            DotNetRestore(_ => _
-                .SetProjectFile(Solution));
-        });
+    Target Restore =>
+        _ => _
+           .Executes(() =>
+            {
+                DotNetRestore(_ => _
+                                 .SetProjectFile(Solution));
+            });
 
-    Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-            DotNetBuild(_ => _
-                .SetProjectFile(Solution)
-                .SetConfiguration(Configuration)
-              //  .SetAssemblyVersion(GitVersion.AssemblySemVer)
-               // .SetFileVersion(GitVersion.AssemblySemFileVer)
-               // .SetInformationalVersion(GitVersion.InformationalVersion)
-                .EnableNoRestore());
-        });
+    Target Compile =>
+        _ => _
+            .DependsOn(Restore)
+            .Executes(() =>
+             {
+                 DotNetBuild(_ => _
+                                 .SetProjectFile(Solution)
+                                 .SetConfiguration(Configuration)
+                                 .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                                 .SetFileVersion(GitVersion.AssemblySemFileVer)
+                                 .SetInformationalVersion(GitVersion.InformationalVersion)
+                                 .EnableNoRestore());
+             });
 
+    AbsolutePath PackageDirectory => OutputDirectory / "packages";
+
+    Target Test =>
+        _ => _
+            .DependsOn(Compile)
+            .Executes(() =>
+             {
+                 DotNetTest(_ => _
+                                .SetConfiguration(Configuration)
+                                .SetNoBuild(InvokedTargets.Contains(Compile))
+                                .ResetVerbosity());
+             });
+
+    Target Pack =>
+       _ => _
+           .DependsOn(Test)
+           .Produces(PackageDirectory / "*.nupkg")
+           .Executes(() =>
+           {
+               DotNetPack(s => s
+                               .SetProject(Solution)
+                               .SetOutputDirectory(PackageDirectory));
+           });
+
+
+    Target Push =>
+        _ => _
+            .DependsOn(Pack)
+            .Executes(() =>
+             {
+                 var packages = PackageDirectory.GlobFiles("*.nupkg");
+                 DotNetNuGetPush(_ => _
+                                     .SetSource(Source)
+                                     .SetApiKey(NugetAPIKey)
+                                     .CombineWith(packages, (_, v) => _
+                                                     .SetTargetPath(v)),
+                                 5,
+                                 true);
+             });
+
+    public static int Main() => Execute<Build>(x => x.Push);
 }
